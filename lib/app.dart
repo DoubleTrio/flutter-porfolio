eef7759a54e019df66fc25d428cf963a60457e81
DoubleTrio <lakacey03@gmail.com>
2021-02-20 23:21:33 -0500

    Testing bloc counter

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+import 'counter/counter.dart';
+
+class CounterApp extends MaterialApp {
+  /// {@macro counter_app}
+  const CounterApp({Key key}) : super(key: key, home: const CounterPage());
+}
